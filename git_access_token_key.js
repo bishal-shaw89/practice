0fc7aa2e360170b7843a5559f139f5796da244cc
Bishal-Tier5 <bishal.shaw@tier5.in>
2023-04-29 13:29:18 +0530

    sample commit to check ownership

--- git_access_token_key.js
+++ git_access_token_key.js
@@ -6,7 +6,7 @@ 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
 // cluster link:
 // mongodb+srv://bishalshaw:WOhr2CwRkjHfuK9c@node.qd8frbo.mongodb.net/shop
 
-// mailchimp api key:
+// mailchimp api key: 
 // 7737925638cdf6e8c2d53d0b9cffa38d-us10
 
 // mailchimp unique id:
